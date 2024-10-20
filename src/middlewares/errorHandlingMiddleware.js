367e6b20d93d9cf938a0261f46c6934b8a0f1221
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2024-10-20 11:06:20 +0700

    Mode Dev_Production With cross-env

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes'
-// import { env } from '~/config/environment'
+import { env } from '~/config/environment'
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
   // trường hợp dev không cẩn thận không thiết lập statusCode thì mặc định trả về stastusCode 500
@@ -12,7 +12,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
   }
 
   // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi.
-  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+  if (env.BUILD_MODE !== 'dev') delete responseError.stack
 
   // Đoạn này có thể mở rộng nhiều về sau như ghi Error Log vào file, bắn thông báo lỗi vào group Slack, Telegram, Email...vv Hoặc có thể viết riêng Code ra một file Middleware khác tùy dự án.
 
