b403471cafe0581b8460769fc22e690ae937c70a
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2024-10-20 17:47:40 +0700

    API Get Details Of Board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -13,6 +13,18 @@ const createNewBoard = async (req, res, next) => {
   } catch (error) { next(error) }
 }
 
+const getDetails = async (req, res, next) => {
+  try {
+    // console.log('req.params: ', req.params)
+    const boardId = req.params.id
+
+    const board = await boardService.getDetails(boardId)
+
+    res.status(StatusCodes.CREATED).json(board)
+  } catch (error) { next(error) }
+}
+
 export const boardController = {
-  createNewBoard
+  createNewBoard,
+  getDetails
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatter'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNewBoard = async (boardData) => {
   try {
@@ -22,6 +24,21 @@ const createNewBoard = async (boardData) => {
   }
 }
 
+const getDetails = async (boardId) => {
+  try {
+    // Gọi đến modal để xử lý lưu bản ghi newBoard
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'board not found')
+    }
+
+    return board
+  } catch (error) {
+    throw error
+  }
+}
+
 export const boardService = {
-  createNewBoard
+  createNewBoard,
+  getDetails
 }
