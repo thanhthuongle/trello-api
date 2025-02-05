0781dc83724ce466fef670d3f95dd6a89854b125
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2025-02-05 22:41:08 +0700

    Update CORS not allow localhost

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,11 +6,12 @@ import ApiError from '~/utils/ApiError'
 // Cấu hình CORS Option trong dự án thực tế
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if (!origin && env.BUILD_MODE === 'dev') {
+    // Nếu là môi trường dev thì cho qua luôn
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
+
+    // Ngược lại thì hiện tại code chúng ta còn 1 môi trường nữa là Production
 
     // Kiểm tra dem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173' // Không cần localhost vì chúng ta đã luôn luôn cho phép môi trường dev vượt qua cors
 ]
 
 export const BOARD_TYPES = {
