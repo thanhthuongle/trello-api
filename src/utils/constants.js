0757695ff8c9f71fdaa25ede83bf49b3fd109900
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2025-02-05 23:51:04 +0700

    Update WhiteList Domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost vì chúng ta đã luôn luôn cho phép môi trường dev vượt qua cors
+  'https://trello-web-beta-three.vercel.app/',
+  'https://trello-web-su-gia-hoa-binhs-projects-a8f56a9c.vercel.app/',
+  'https://trello-web-git-master-su-gia-hoa-binhs-projects-a8f56a9c.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
