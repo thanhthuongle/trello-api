5c49cbbe18590826aeec40c59308cfcea33da5bd
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2025-02-12 10:48:27 +0700

    Logout and Refresh Token

--- src/services/userService.js
+++ src/services/userService.js
@@ -88,16 +88,39 @@ const login = async (reqBody) => {
     const userInfo = { _id: existUser._id, email: existUser.email }
 
     // Tạo ra 2 loại token, accessToken-và-refreshToken- để trả về cho phía - FE
-    const accessToken = await JwtProvider.generateToken(userInfo, env.ACCESS_TOKEN_SECRET_SIGNATURE, env.ACCESS_TOKEN_LIFE)
-    const refreshToken = await JwtProvider.generateToken(userInfo, env.REFRESH_TOKEN_SECRET_SIGNATURE, env.REFRESH_TOKEN_LIFE)
+    const accessToken = await JwtProvider.generateToken(userInfo, env.ACCESS_TOKEN_SECRET_SIGNATURE, 5)
+    const refreshToken = await JwtProvider.generateToken(userInfo, env.REFRESH_TOKEN_SECRET_SIGNATURE, 15)
 
     // Trả về thông tin của user kèm theo 2 cái token - vừa tạo ra
     return { accessToken, refreshToken, ...pickUser(existUser) }
   } catch (error) { throw error }
 }
 
+const refreshToken = async (clientRefreshToken) => {
+  try {
+    // Verify / giải mã cái refresh token xem có hợp lệ không
+    const refreshTokenDecoded = await JwtProvider.verifyToken(clientRefreshToken, env.REFRESH_TOKEN_SECRET_SIGNATURE)
+
+    // Đoạn này vì chúng ta chỉ lưu những thông tin unique và cố định của user trong token rồi, vì vậy có thể lấy luôn từ decoded ra, tiết kiệm query vào DB để lấy data mới.
+    const userInfo = {
+      _id: refreshTokenDecoded._id,
+      email: refreshTokenDecoded.email
+    }
+
+    // Tạo accessToken mới
+    const accessToken = await JwtProvider.generateToken (
+      userInfo,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      5
+    )
+
+    return { accessToken }
+  } catch (error) { throw error }
+}
+
 export const userService = {
   createNew,
   verifyAccount,
-  login
+  login,
+  refreshToken
 }
